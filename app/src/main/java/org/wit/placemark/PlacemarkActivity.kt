aaf4d7045edaab54e95db62b45d3c596f9d29d5b
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-09-12 12:09:02 +0100

    First commit

---
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.placemark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacemarkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.placemark_main)
+    }
+}
