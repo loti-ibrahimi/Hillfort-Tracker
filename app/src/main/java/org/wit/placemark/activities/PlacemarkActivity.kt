60df8f4f04f0724b7edc575f6c858e7f11b6645c
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-09-23 13:48:22 +0100

    Reference to MainApp object

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,28 +7,33 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     var placemark = PlacemarkModel()
-    //Exercise 3: Create ArrayList.
-    val placemarks = ArrayList<PlacemarkModel>()
+    // Reference to MainApp object
+    var app :  MainApp? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
-        info("Placemark Activity started...")
+        // Initialisation of MainApp object.
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
             placemark.title = placemarkTitle.text.toString()
             placemark.description = placemarkDescription.text.toString()
 
             if(placemark.title.isNotEmpty() && placemark.description.isNotEmpty()) {
+                // MainApp object being used.
+                // '!!' returns output as non-null & throws exception otherwise.
+                app!!.placemarks.add(placemark.copy())
                 info("[Add] Button Pressed: $placemarkTitle")
-                // Populate 'placemarks' array & log contents.
-                placemarks.add(placemark);
-                info("All Placemarks: $placemarks")
+                for (i in app!!.placemarks.indices) {
+                    info("Placemark[$i]:${app!!.placemarks[i]}")
+                }
             }
             else {
                 toast ("Please Enter a title & description")
