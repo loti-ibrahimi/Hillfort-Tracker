94585ad5b6d75e6e1d961370a5b5bf8ef9f7d5d5
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-09-30 12:31:46 +0100

    Reworked to include RecyclerView & Adapter

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -1,9 +1,17 @@
 package org.wit.placemark.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_placemark_list.*
+import kotlinx.android.synthetic.main.card_placemark.view.*
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkListActivity : AppCompatActivity() {
 
@@ -13,5 +21,38 @@ class PlacemarkListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark_list)
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+    }
+}
+
+class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) :
+    RecyclerView.Adapter<PlacemarkAdapter.MainHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        return MainHolder(
+            LayoutInflater.from(parent?.context).inflate(
+                R.layout.card_placemark,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val placemark = placemarks[holder.adapterPosition]
+        holder.bind(placemark)
+    }
+
+    override fun getItemCount(): Int = placemarks.size
+
+    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(placemark: PlacemarkModel) {
+            itemView.placemarkTitle.text = placemark.title
+            itemView.description.text = placemark.description
+        }
     }
 }
