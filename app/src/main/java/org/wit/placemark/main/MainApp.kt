f89a897347a22b8b9698963d73bea742ead5a231
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-10-07 12:45:22 +0100

    Reworked MainApp to use PlacemarkMemStore class

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,11 +3,14 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.placemark.models.PlacemarkMemStore
 import org.wit.placemark.models.PlacemarkModel
 
 class MainApp : Application(), AnkoLogger {
 
-    val placemarks = ArrayList<PlacemarkModel>()
+    // val placemarks = ArrayList<PlacemarkModel>()
+    // Use the PlacemarkMemStore class
+    val placemarks = PlacemarkMemStore()
 
     override fun onCreate() {
         super.onCreate()
