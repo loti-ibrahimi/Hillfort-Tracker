6e5152dada2248361a3467c722998d9e8f00a1dc
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-11-03 16:36:25 +0000

    Updated Model

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -7,9 +7,11 @@ data class PlacemarkModel(var id: Long = 0,
                           var title: String = "",
                           var description: String = "",
                           var image: String = "",
+                          var visited: Boolean = false,
                           var lat : Double = 0.0,
                           var lng: Double = 0.0,
                           var zoom: Float = 0f) : Parcelable
+
 @Parcelize
 data class Location(var lat: Double = 0.0,
                     var lng: Double = 0.0,
