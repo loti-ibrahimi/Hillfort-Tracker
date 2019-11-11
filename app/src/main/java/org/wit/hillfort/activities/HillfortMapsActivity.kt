cdd9130e6928f61216859a3c0a196974576a9bf0
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-11-11 13:08:37 +0000

    Refactored Maps Activity

--- app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortMapsActivity.kt
@@ -1,7 +1,6 @@
 package org.wit.hillfort.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import org.wit.hillfort.R
 
@@ -12,12 +11,7 @@ class HillfortMapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort_maps)
+        toolbar.title = title
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
-
 }
