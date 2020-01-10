5651b9bdc116eff2e35f536ddad34de99382eaed
loti-ibrahimi <loti.ibrahimi@outlook.com>
2020-01-10 08:29:53 +0000

    Database/Firebase Functionality established

--- app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -22,7 +22,7 @@ class HillfortView : BaseView(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hillfort)
-        super.init(toolbarAdd)
+        super.init(toolbarAdd, true)
 
         presenter = initPresenter (HillfortPresenter(this)) as HillfortPresenter
 
--- app/src/main/java/org/wit/hillfort/views/login/LoginPresenter.kt
+++ app/src/main/java/org/wit/hillfort/views/login/LoginPresenter.kt
@@ -2,6 +2,7 @@ package org.wit.hillfort.views.login
 
 import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.toast
+import org.wit.hillfort.models.firebase.HillfortsFireStore
 import org.wit.hillfort.views.BasePresenter
 import org.wit.hillfort.views.BaseView
 import org.wit.hillfort.views.VIEW
@@ -9,16 +10,31 @@ import org.wit.hillfort.views.VIEW
 class LoginPresenter(view: BaseView) : BasePresenter(view) {
 
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    var fireStore: HillfortsFireStore? = null
+
+    init {
+        if (app.hillforts is HillfortsFireStore) {
+            fireStore = app.hillforts as HillfortsFireStore
+        }
+    }
 
     fun doLogin(email: String, password: String) {
         view?.showProgress()
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
             if (task.isSuccessful) {
-                view?.navigateTo(VIEW.LIST)
+                if (fireStore != null) {
+                    fireStore!!.fetchPlacemarks {
+                        view?.hideProgress()
+                        view?.navigateTo(VIEW.LIST)
+                    }
+                } else {
+                    view?.hideProgress()
+                    view?.navigateTo(VIEW.LIST)
+                }
             } else {
+                view?.hideProgress()
                 view?.toast("Sign Up Failed: ${task.exception?.message}")
             }
-            view?.hideProgress()
         }
     }
 
@@ -26,11 +42,12 @@ class LoginPresenter(view: BaseView) : BasePresenter(view) {
         view?.showProgress()
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
             if (task.isSuccessful) {
+                view?.hideProgress()
                 view?.navigateTo(VIEW.LIST)
             } else {
+                view?.hideProgress()
                 view?.toast("Sign Up Failed: ${task.exception?.message}")
             }
-            view?.hideProgress()
         }
     }
 }
